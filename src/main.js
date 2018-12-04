d0d327e5c42eba33ef8a492e4859d8f48dd0f51e
pokio <32353873+pokiiio@users.noreply.github.com>
2018-12-04 16:56:53 +0900

    Add multi-screen support.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-let mainWindow;
+let windows = [];
 
 app.on('window-all-closed', function () {
   if (process.platform != 'darwin') {
@@ -12,9 +12,46 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
-  var size = electron.screen.getPrimaryDisplay().size;
+  showWindows();
 
-  mainWindow = new BrowserWindow({
+  electron.screen.on('display-added', function () {
+    dismissWindows();
+    showWindows();
+  });
+
+  electron.screen.on('display-removed', function () {
+    dismissWindows();
+    showWindows();
+  });
+});
+
+function showWindows() {
+  electron.screen.getAllDisplays().forEach(display => {
+    windows.push(showWindow(display));
+  });
+}
+
+function dismissWindows() {
+  if (!windows || windows.length < 1) {
+    return;
+  }
+
+  windows.forEach(window => {
+    if (!window) {
+      return;
+    }
+
+    window.close();
+  });
+
+  windows = [];
+}
+
+function showWindow(display) {
+  let size = display.size;
+  let window = new BrowserWindow({
+    x: display.bounds.x,
+    y: display.bounds.y,
     width: size.width,
     height: size.height,
     frame: false,
@@ -23,10 +60,12 @@ app.on('ready', function () {
     alwaysOnTop: true
   });
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.setIgnoreMouseEvents(true);
+  window.loadURL('file://' + __dirname + '/index.html');
+  window.setIgnoreMouseEvents(true);
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
+  window.on('closed', function () {
+    window = null;
   });
-});
+
+  return window;
+}
